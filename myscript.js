00f35929bbcdea2184ff84883def5aaae487c8bb
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-11 20:00:49 +0100

    aggiunte immagini grandi con descrizione

--- myscript.js
+++ myscript.js
@@ -25,10 +25,11 @@ const text = [
 
 //definisco le variabili
 const ITEMWRAPPER = document.querySelector(".item-wrapper");
+const SLIDEWRAPPER = document.querySelector(".slide-wrapper");
 
 
 
-//ciclo for per inserire img
+//ciclo for per inserire img slider
 for (let i = 0; i < items.length; i++) {
     const ITEM = document.createElement("div");
     ITEMWRAPPER.append(ITEM);
@@ -42,4 +43,24 @@ for (let i = 0; i < items.length; i++) {
 console.log(ITEMWRAPPER)
 
 
+//ciclo for per inserire img grande
+for (let i = 0; i < items.length; i++) {
+    const ITEM = document.createElement("div");
+    SLIDEWRAPPER.append(ITEM);
+    ITEM.classList.add("slide-" + (i) , "slide" );
 
+    let immagini = document.createElement("img");
+    immagini.src = items[i];
+    ITEM.append(immagini);
+
+    let titolo = document.createElement("h1");
+    ITEM.append(titolo);
+    titolo.innerHTML += title[i]
+
+    let testo = document.createElement("h4");
+    ITEM.append(testo);
+    testo.innerHTML += text[i]
+}
+
+
+console.log(SLIDEWRAPPER)
